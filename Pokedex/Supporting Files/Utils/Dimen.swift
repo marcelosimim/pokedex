77ffdb2b9403d6633f8f721e1119cc903fd4eebd
Marcelo <marcelosimim@gmail.com>
2022-08-01 15:32:16 -0300

    dimen file

---
+++ Pokedex/Supporting Files/Utils/Dimen.swift
@@ -0,0 +1,17 @@
+//
+//  Dimen.swift
+//  Pokedex
+//
+//  Created by Marcelo Simim Santos on 8/1/22.
+//
+
+import Foundation
+import UIKit
+
+class Dimen {
+    public static let deviceHeight = UIScreen.main.bounds.height
+    public static let deviceWidth = UIScreen.main.bounds.width
+    public static let heightMultiplier: CGFloat = deviceHeight/812
+    public static let widthMultiplier: CGFloat = deviceWidth/375
+    public static let leadingMargin: CGFloat = 24*widthMultiplier
+}
